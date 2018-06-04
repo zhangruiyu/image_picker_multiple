ea67f447c5729783a61f68e175b44c5034e0ca0f
zhangruiyu <zhangruiyu@hexindai.com>
2018-06-04 17:56:55 +0800

    init

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,12 +34,12 @@ class _MyAppState extends State<MyApp> {
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
-    if (!mounted) return;
+  if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion.toString();
-    });
-  }
+  setState(() {
+  _platformVersion = platformVersion.toString();
+  });
+}
 
   @override
   Widget build(BuildContext context) {
